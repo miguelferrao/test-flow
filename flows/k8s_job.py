2e7f946b24d259ea57475bc129d5911c3d43c5ac
miguelferrao <109652378+miguelferrao@users.noreply.github.com>
2022-08-01 11:45:15 +0200

    Add files via upload

---
+++ flows/k8s_job.py
@@ -0,0 +1,23 @@
+from prefect import task, Flow
+from prefect.tasks.kubernetes.job import CreateNamespacedJob
+import yaml
+from yaml import SafeLoader
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GitHub
+from git import Repo
+
+
+@task
+def job1():
+    with open('job.yaml') as f:
+        data = yaml.load(f, Loader=SafeLoader)
+        CreateNamespacedJob(body=data, kubernetes_api_key_secret=None).run()
+    
+
+with Flow(name="job-flow-1", run_config=KubernetesRun()) as flow:
+    task = job1()
+
+flow.storage = GitHub(
+    repo="miguelferrao/test-flow",
+    path="k8s_job.py",       
+)
